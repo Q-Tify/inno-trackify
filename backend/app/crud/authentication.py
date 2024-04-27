bf03d387ec3bb525c501e5a42efc73c841acde12
Vladislav Spigin <v.spigin@tinkoff.ru>
2024-04-27 19:30:32 +0300

    lint

--- backend/app/crud/authentication.py
+++ backend/app/crud/authentication.py
@@ -8,19 +8,30 @@ from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 from ..config import SECRET_KEY, ALGORITHM, EXPIRE_TIME_MINUTES
 
+
 def get_user_username_password(db: Session, username: str, password: str):
-    return db.query(models.User).filter(models.User.username == username, models.User.password == password).first()
+    return (
+        db.query(models.User)
+        .filter(
+            models.User.username == username, models.User.password == password
+        )
+        .first()
+    )
+
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=EXPIRE_TIME_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=EXPIRE_TIME_MINUTES
+        )
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 def get_username_from_token(token: str):
     payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-    return payload.get("sub") 
+    return payload.get("sub")
